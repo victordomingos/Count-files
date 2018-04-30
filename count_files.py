8e6d5aafd2750af57889b5906eb2da30b6f2d689
Victor Domingos <victordomingos@users.noreply.github.com>
2018-04-30 10:52:32 +0100

    OOP

--- count_files.py
+++ count_files.py
@@ -5,61 +5,64 @@ import argparse
 from pprint import pprint
 
 
-parser = argparse.ArgumentParser(description='\nRecursively count all files in a directory, by file extension.')
-
-parser.add_argument('path',
-                    type=str,
-                    help='The path to the folder containing the files to be counted.')
-
-parser.add_argument('-nr',
-                    action='store_true',
-                    help="Don't recurse through subdirectories")
-
-args = parser.parse_args()
-
-filecount = dict()
-
-
-if args.path:
-    location = os.path.expanduser(args.path)
-    loc_text = ':\n' + location
-else:
-    location = os.getcwd()
-    loc_text = ' current directory'
-    
-if args.nr:
-    recursive = False
-    msg = f'\nCounting files in{loc_text}.\n'
-    print(msg)
-    
-    for f in os.listdir(location):
-        filename_parts = f.split('.')
-        if len(filename_parts) == 1:
-            extension = '[no extension]'
-        else:
-            extension = filename_parts[-1]
-            
-        if extension in filecount.keys():
-            filecount[extension] += 1
-        else:
-            filecount[extension] = 1
-            
-else:
-    recursive = True
-    msg = f'Recursively counting all files in:\n{loc_text}.\n'
-    print(msg)
-    
-    for root, dirs, files in os.walk(location):
-        for f in files:
-            filename_parts = f.split('.')
-            if len(filename_parts) == 1:
-                extension = '[no extension]'
-            else:
-                extension = filename_parts[-1]
-                
-            if extension in filecount.keys():
-                filecount[extension] += 1
-            else:
-                filecount[extension] = 1
+def get_file_extension(file_path: str) -> str:
+    filename_parts = file_path.split('.')
+    if len(filename_parts) == 1:
+        extension = '[no extension]'
+    else:
+        extension = filename_parts[-1]
+    return extension
         
-pprint(filecount)
+
+class WordCounter:
+    def __init__(self):
+        self.counters = dict()
+        
+    def count_word(self, word:str):
+        if word in self.counters.keys():
+            self.counters[word] += 1
+        else:
+            self.counters[word] = 1           
+
+       
+if __name__ == "__main__":  
+    parser = argparse.ArgumentParser(description='\nRecursively count all files in a directory, by file extension.')
+    
+    parser.add_argument('path',
+                        type=str,
+                        help='The path to the folder containing the files to be counted.')
+    
+    parser.add_argument('-nr',
+                        action='store_true',
+                        help="Don't recurse through subdirectories")
+    
+    args = parser.parse_args()
+    recursive = not args.nr
+    
+    fc = WordCounter()
+        
+    if args.path:
+        location = os.path.expanduser(args.path)
+        loc_text = ':\n' + location
+    else:
+        location = os.getcwd()
+        loc_text = ' current directory'
+    
+                            
+    if recursive:  
+        print(f'Recursively counting all files in:\n{loc_text}.\n')
+        
+        for root, dirs, files in os.walk(location):
+            for f in files:
+                extension = get_file_extension(f)   
+                fc.count_word(extension)
+        
+    else: 
+        print(f'\nCounting files in{loc_text}.\n')
+        
+        for f in os.listdir(location):
+            extension = get_file_extension(f)   
+            fc.count_word(extension)
+    
+        
+    pprint(fc.counters)
