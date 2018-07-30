19f2efb5ff6c3a1d3e2c56db4f570a10cfa84352
NataliaBondarenko <write_up@mail.ua>
2018-07-30 17:14:34 +0300

    script to run all tests

---
+++ tests/test_runner.py
@@ -0,0 +1,26 @@
+import unittest
+from tests.test_argument_parser import TestArgumentParser
+from tests.test_some_functions import TestSomeFunctions
+from tests.test_viewing_modes import TestViewingModes
+
+
+def suite():
+    """Run all tests except TestViewingModes.test_show_result_for_search_files.
+    :return:
+    """
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(TestArgumentParser))
+    test_suite.addTest(unittest.makeSuite(TestSomeFunctions))
+    test_suite.addTest(TestViewingModes('test_show_2columns'))
+    return test_suite
+
+
+# run all suite tests from root directory:
+# python tests/test_runner.py
+
+# or run file in PyCharm
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
