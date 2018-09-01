513d4ea04a89698beec78326a85568a9c827b59c
victordomingos <editor.arcosonline@gmail.com>
2018-09-01 14:53:41 +0100

    trying to improve user experience on iOS

--- count_files/settings.py
+++ count_files/settings.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 import shutil
-
+import platform
 
 BUG_REPORT_URL = 'https://github.com/victordomingos/Count-files/issues'
 
-TERM_WIDTH, _ = shutil.get_terminal_size((80, 24))
+TERM_WIDTH = shutil.get_terminal_size((80, 24)).columns
 DEFAULT_PREVIEW_SIZE = 5 * TERM_WIDTH  # 5 lines of text preview
 START_TEXT_WIDTH = min(TERM_WIDTH, 100)
 DEFAULT_EXTENSION_COL_WIDTH = 9
 DEFAULT_FREQ_COL_WIDTH = 5
 MAX_TABLE_WIDTH = 80
+
+# ====================[ iOS/Pythonista specific settings ]====================
+IPAD_FONT_SIZE = 15
+IPHONE_FONT_SIZE = 10
+IOS_WORKERS = 2
+IOS_FONT = "Menlo"
+
+if platform.system() == 'Darwin':
+        if platform.machine().startswith('iPad'):
+            device = "iPad"
+        elif platform.machine().startswith('iP'):
+            device = "iPhone"
+        else:
+            device = "mac"
+    else:
+        device = "other"
+
+    if device in ("iPad", "iPhone"):
+        # Adapt for smaller screen sizes in iPhone and iPod touch
+        import ui
+        import console
+        if device == 'iPad':
+            font_size = IPAD_FONT_SIZE
+        else:
+            font_size = IPHONE_FONT_SIZE
+        console.set_font(IOS_FONT, font_size)
+        screen_width = ui.get_screen_size().width
+        char_width = ui.measure_string('.', font=(IOS_FONT, font_size)).width
+        TERM_WIDTH = int(screen_width / char_width - 1.5) - 1
+
 
 SUPPORTED_TYPES = {
     'all_extensions': ['..'],
