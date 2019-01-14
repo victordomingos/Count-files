68644cf0bfa306e7670d5a75c38ec6a720c46fff
NataliaBondarenko <write_up@mail.ua>
2019-01-14 19:44:36 +0200

    new def for printing help text, updated def search_in_help with new sections

--- count_files/utils/help_system_extension.py
+++ count_files/utils/help_system_extension.py
@@ -3,68 +3,66 @@
 """
 from textwrap import fill
 from itertools import chain
-from typing import List
 
-from count_files.utils.help_text import help_system_message_general, \
-   help_system_message_list, arguments, group_names, search_words, indexes
-from count_files.utils import help_text
+from count_files.utils.help_text import indexes, docs_text, docs_groups_text, \
+    docs_sort_text, docs_args_text, docs_list_text, docs_general_text
 from count_files.settings import START_TEXT_WIDTH
 
 
-def print_help_message(title: str, help_system_message: str or List[dict]):
-    """Print an adaptive and formatted help message.
+def print_help_text(text: str):
+    """Print an adaptive and formatted help text for section in docs.
 
-    :param title: headline
-    :param help_system_message: string or List[dict] with args example and description
+    Sections:
+    count-files --args-help docs [list, args, sort, groups]
+    :param text: section text
     :return:
     """
-    if isinstance(help_system_message, str):
-        print(fill(title, width=START_TEXT_WIDTH, initial_indent=' ' * 2, subsequent_indent=' ' * 2))
-        print(fill(help_system_message, width=START_TEXT_WIDTH, initial_indent=' ' * 6, subsequent_indent=' ' * 6))
-        return
-    else:
-        print(fill(title, width=START_TEXT_WIDTH, initial_indent=' ' * 2, subsequent_indent=' ' * 2))
-        for item in help_system_message:
-            print(fill(item['desc'], width=START_TEXT_WIDTH, initial_indent=' ' * 2, subsequent_indent=' ' * 2))
-            print(fill(item['args'], width=START_TEXT_WIDTH, initial_indent=' ' * 6, subsequent_indent=' ' * 6))
-        return
+    for item in text.split('\n'):
+        print(fill(item, width=START_TEXT_WIDTH, initial_indent=' ' * 2, subsequent_indent=' ' * 2))
+    return
 
 
-def search_in_help(keyword: str):
-    """Search for help text by keyword(argument or group name, search words).
+def search_in_help(topic: str):
+    """Search for help text by topic(argument or group name, search words).
 
     Display corresponding help message for:
     count-files --args-help docs
-        print help_text.__doc__
-        this HELP SYSTEM EXTENSION DOCS from count_files.utils.help_text.py
+        basic usage examples, this HELP SYSTEM EXTENSION DOCS
     count-files --args-help list
-        print arguments, group_names, search_words from count_files.utils.help_text.py
-    count-files --args-help <keyword>
+        available arguments, group_names, search_words
+    count-files --args-help args
+        more about search by short/long argument name
+    count-files --args-help sort
+        more about sorting arguments by purpose or type
+    count-files --args-help groups
+        more about sorting arguments by group.
+    count-files --args-help <topic>
         searching or sorting using indexes from count_files.utils.help_text.py
         default: show long description for <keyword in lower case>,
         show short description for <keyword with all or one letter in upper case>
-    :param keyword: argument or group name, search words in lower or upper case
+    :param topic: argument or group name, search words in lower or upper case
     :return:
     """
-    key_lower = keyword.lower()
+    key_lower = topic.lower()
     if key_lower == 'docs':
-        for item in help_text.__doc__.split('\n'):
-            print(fill(item,  width=START_TEXT_WIDTH, initial_indent=' ' * 2, subsequent_indent=' ' * 2))
+        print_help_text(docs_text)
+    elif key_lower == 'args':
+        print_help_text(docs_args_text)
+    elif key_lower == 'sort':
+        print_help_text(docs_sort_text)
+    elif key_lower == 'groups':
+        print_help_text(docs_groups_text)
     elif key_lower == 'list':
-        print('  KEYWORDS:')
-        print_help_message('Available arguments:', ', '.join(arguments))
-        print_help_message('Available group names:', ', '.join(group_names))
-        print_help_message('Available search words:', ', '.join(search_words))
-        print_help_message('ALSO USE:', help_system_message_list)
+        print_help_text(docs_list_text)
     elif key_lower not in set(chain.from_iterable(indexes.keys())):
-        print(fill(f'Not found: {keyword}', width=START_TEXT_WIDTH, initial_indent=' ' * 2, subsequent_indent=' ' * 2))
-        print_help_message('ALSO USE:', help_system_message_general)
+        print(fill(f'Not found: {topic}', width=START_TEXT_WIDTH, initial_indent=' ' * 2, subsequent_indent=' ' * 2))
+        print_help_text(docs_general_text)
     else:
         for k, v in indexes.items():
             if key_lower in k:
                 print(fill(str(v[0]), width=START_TEXT_WIDTH, initial_indent=' ' * 2, subsequent_indent=' ' * 2))
                 # show long description
-                if keyword == key_lower:
+                if topic == key_lower:
                     print(fill(f'Long: {v[2]}', width=START_TEXT_WIDTH, initial_indent=' ' * 6, subsequent_indent=' ' * 6))
                 # show short description
                 else:
