dd587dc15dbbe9b38b842f37edbe2e31752ddbdc
NataliaBondarenko <write_up@mail.ua>
2020-03-18 20:44:56 +0200

    added def group_ext_by_type

--- count_files/utils/file_handlers.py
+++ count_files/utils/file_handlers.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 import os
 from itertools import chain
+from typing import List, Tuple, Dict
 
 from count_files.settings import SUPPORTED_TYPES
 
@@ -36,3 +37,37 @@ def is_supported_filetype(extension: str) -> bool:
     :return: True if we have a preview procedure for the given file type, False otherwise.
     """
     return extension in list(chain.from_iterable(SUPPORTED_TYPES.values()))
+
+
+def group_ext_by_type(data: List[Tuple[str, int]],
+                      ext_and_group: Dict[str, str]) -> Dict[str, List[Tuple[str, int]]]:
+    """Group file extensions by type.
+
+    Default ext_and_group:
+    archives, audio, audio/video, data, documents, executables, fonts, images,
+    Python related extensions, videos, and other files.
+    User-defined ext_and_group:
+    may be created from configuration file.
+    Initial storage: dict with items like {'documents': [], 'images': [], ...}
+    key - group name, value - empty list.
+    'other': reserved key.
+    :param data: list with items like [('png', 8), ('txt', 25), ...]
+    :param ext_and_group: dict with items like {'png': 'image', 'txt': documents, ...}
+    :return: sorted dict with items like {'documents': [('txt', 25), ...], 'images': [('png', 8), ...], ...}
+    """
+    # storage: Dict[str, list]
+    # local scope important
+    storage = dict.fromkeys(sorted(set(ext_and_group.values())), [])
+    other = []
+    for (ext, freq) in data:
+        item_type = ext_and_group.get(ext.lower(), 'other')
+        if item_type == 'other':
+            other.append((ext, freq))
+        else:
+            if storage.get(item_type):
+                storage[item_type].append((ext, freq))
+            else:
+                storage[item_type] = [(ext, freq)]
+    if other:
+        storage.update({'other': other})
+    return storage
